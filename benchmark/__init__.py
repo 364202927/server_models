c000a056f35cfba8c0932701a3e59c664dfc7276
Agzer0 <364202927@qq.com>
2026-09-22 00:25:13 +0800

    去掉这部分测试模型的代码

--- benchmark/__init__.py
+++
@@ -1,6 +0,0 @@
-from .runner import BenchmarkRunner, BenchmarkResult
-from .scorer import Scorer
-from .report import ReportGenerator
-from .engine import load_cases, run_cases
-
-__all__ = ["BenchmarkRunner", "BenchmarkResult", "Scorer", "ReportGenerator", "load_cases", "run_cases"]
